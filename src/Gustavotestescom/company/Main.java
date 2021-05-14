e55867e2d8bd24e7f6e6d3421bbae1a455642aef
Gustavo0707 <gustavomelo0703@gmail.com>
2021-05-14 15:16:54 -0300

    Initial commit

---
+++ src/Gustavotestescom/company/Main.java
@@ -0,0 +1,13 @@
+package Gustavotestescom.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+       int i = 1;
+
+        while(i<=20){
+            System.out.println(i);
+            ++i;
+        }
+    }
+}
